8bbffd24808c3394aee34b82580e019eb3703fa3
John B. Hall <jhall@customink.com>
2018-09-14 16:34:20 -0400

    add bash_profile

---
+++ bash_profile.sh
@@ -0,0 +1,15 @@
+eval "$(rbenv init -)"
+
+PS1="\w\n$ "
+alias be="bundle exec"
+
+export EDITOR="atom"
+
+# added 2/20/18 for NDX
+eval "$(nodenv init -)"
+
+# strap!
+STRAP_BIN_DIR=~/github/customink/strap/bin
+if [ -d $STRAP_BIN_DIR ]; then
+  PATH="$STRAP_BIN_DIR:${PATH}"
+fi
